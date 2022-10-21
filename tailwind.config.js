746dfef88e5e319634567f2721a2fe1e958f9abb
tahaakbulut <34061497+tahaakbulut@users.noreply.github.com>
2022-10-21 06:48:34 +0300

    added router & pretier & tailwind package

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
+  theme: {
+    container: {
+      center: true,
+    },
+    extend: {
+      colors: {
+        primary: '#32a849',
+        dark: '#505254',
+      },
+    },
+  },
+  plugins: [],
+};
